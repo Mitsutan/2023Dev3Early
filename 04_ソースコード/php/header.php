0766a00d0624a8630b14f9223059685c73bf4195
Mitsutan <petitcom_hondou@icloud.com>
2023-05-23 10:31:04 +0900

    header
    <body>の直後に<?php require_once "./php/header.php" ?>と記述することでページにヘッダーを挿入することができます。

---
+++ 04_ソースコード/php/header.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="container-fluid bg-dark p-1">
+        <img src="./img/logo.png" alt="筋肉Memorial">
+    </div>
+    <nav>
+        <div class="container-fluid bg-warning">
+            <ul class="py-2" id="nav_ul">
+                <li><a href=""><i class="fa-solid fa-house"></i>HOME</a></li>
+                <li><a href=""><i class="fa-solid fa-ranking-star"></i>ランキング</a></li>
+                <li><a href=""><i class="fa-solid fa-hashtag"></i>ハッシュタグ</a></li>
+                <li><a href="">いいね</a></li>
+                <li><a href=""><i class="fa-solid fa-magnifying-glass"></i>検索</a></li>
+                <li><a href=""><i class="fa-solid fa-arrow-right-to-bracket"></i>ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
