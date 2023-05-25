dc8b4826d345334b000743cd52ff2e6c2e68fe67
Mitsutan <petitcom_hondou@icloud.com>
2023-05-25 11:26:14 +0900

    Update header.php
    リンク追加（仮）

--- 04_ソースコード/php/header.php
+++ 04_ソースコード/php/header.php
@@ -7,10 +7,10 @@
         <div class="container-fluid bg-warning">
             <ul class="py-2" id="nav_ul">
                 <li><a href="./index.php"><i class="fa-solid fa-house"></i><span class="d-none d-md-inline">HOME</span></a></li>
-                <li><a href=""><i class="fa-solid fa-ranking-star"></i><span class="d-none d-md-inline">ランキング</span></a></li>
-                <li><a href=""><i class="fa-solid fa-hashtag"></i><span class="d-none d-md-inline">ハッシュタグ</span></a></li>
-                <li><a href=""><span class="d-none d-md-inline">いいね</span></a></li>
-                <li><a href=""><i class="fa-solid fa-magnifying-glass"></i><span class="d-none d-md-inline">検索</span></a></li>
+                <li><a href="./ranking.php"><i class="fa-solid fa-ranking-star"></i><span class="d-none d-md-inline">ランキング</span></a></li>
+                <li><a href="./hashtags.php"><i class="fa-solid fa-hashtag"></i><span class="d-none d-md-inline">ハッシュタグ</span></a></li>
+                <li><a href="./goods.php"><i class="fa-solid fa-thumbs-up"></i><span class="d-none d-md-inline">いいね</span></a></li>
+                <li><a href="./search.php"><i class="fa-solid fa-magnifying-glass"></i><span class="d-none d-md-inline">検索</span></a></li>
                 <li><a href="./login.php"><i class="fa-solid fa-arrow-right-to-bracket"></i><span class="d-none d-md-inline">ログイン</span></a></li>
             </ul>
         </div>
