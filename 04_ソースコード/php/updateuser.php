42e246a44f2b3a6b868237f5f81827083a884ef4
Mitsutan <petitcom_hondou@icloud.com>
2023-05-30 09:41:18 +0900

    プロフィール更新処理

--- 04_ソースコード/php/updateuser.php
+++ 04_ソースコード/php/updateuser.php
@@ -7,12 +7,13 @@ $db = new DBManager();
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $mail = $_POST["mail"];
     $introduce = $_POST["Introduce"];
-    $name="a";
+    $name = $_POST["name"];
 
     // プロフィール画像のアップロード処理
     if ($_FILES["avatar"]["name"]) {
-        $avatar = $_FILES["avatar"]["name"];
-        $avatarPath = "./" . $avatar;
+        mkdir("../img/userpics/".$_SESSION["user_id"]);
+        $avatar = "userpic".$_FILES["avatar"]["name"];
+        $avatarPath = "../img/userpics/".$_SESSION["user_id"]."/".$avatar;
         move_uploaded_file($_FILES["avatar"]["tmp_name"], $avatarPath);
     } else {
         $avatar = ""; // もし画像がアップロードされていない場合は、空の値を設定する
@@ -22,5 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $db->editUser($_SESSION["user_id"], $mail,$name, $introduce);
 }
 
-$userData = $db->getUser($_SESSION["user_id"]);
+// $userData = $db->getUser($_SESSION["user_id"]);
+
+header("Location: ../mypage.php");
 ?>
