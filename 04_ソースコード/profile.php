1240a58d7bda89f9e927beb32298469748ec4c78
Mitsutan <petitcom_hondou@icloud.com>
2023-06-01 10:56:12 +0900

    プロフィール画面（仮）
    納得はしていない

--- 04_ソースコード/profile.php
+++ 04_ソースコード/profile.php
@@ -26,36 +26,32 @@ $userData = $db->getUser($_GET["id"]);
 
 <body>
     <?php require_once "./php/header.php" ?>
-    <div class="container-fluid">
-        <h1 style = "margin-top:70px; margin-left:30px;">（ここにユーザー名）</h1>
-        <div class = "row gx-3" style = "height: 400px">
-        <div class = "col-3 d-flex align-items-center">
-            <img src="kawaii.jpg" class="mx-auto d-block" style = "border-radius: 50%; width: 250px; height: 250px;">
+    <div class="container">
+        <h1>（ここにユーザー名）</h1>
+        <div class="row gx-3">
+            <div class="col-3 text-center">
+                <img src="./img/user_default.png" class="rounded-circle" width="250" height="250">
+            </div>
+            <div class="col-9">
+                <?php
+                $showIntroduction = true; // 紹介文を表示するかどうかの条件
+                if ($showIntroduction) {
+                    echo "<div>（ここに自己紹介文）</div>";
+                } else {
+                    // 紹介文を表示しない場合の処理
+                }
+                ?>
+            </div>
         </div>
         <div class="follower-box">
             <div class="follow-count">フォロー:</div>
             <div class="follower-count">フォロワー:</div>
-        
-
-    <div class ="col-9">
-    <?php
-        $showIntroduction = true; // 紹介文を表示するかどうかの条件
-        if ($showIntroduction) {
-            echo "<div>（ここに自己紹介文）</div>";
-        } else {
-            // 紹介文を表示しない場合の処理
-        }
-    ?>
-    </div>
-    </div>
-    
-
-            </div>
         </div>
+    </div>
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <script src="./script/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="./script/script.js"></script>
 </body>
 
 </html>
