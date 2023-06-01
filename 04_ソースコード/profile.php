fb18037d8b96a30980975758b6bd1463a7ea47c8
Mitsutan <petitcom_hondou@icloud.com>
2023-06-01 11:01:18 +0900

    Update profile.php
    画像を反映

--- 04_ソースコード/profile.php
+++ 04_ソースコード/profile.php
@@ -4,7 +4,7 @@ session_start();
 require_once "./php/DBManager.php";
 $db = new DBManager;
 
-// $userData = $db->getUser($_GET["id"]);
+$userData = $db->getUser($_GET["id"]);
 
 ?>
 <!DOCTYPE html>
@@ -27,16 +27,23 @@ $db = new DBManager;
 <body>
     <?php require_once "./php/header.php" ?>
     <div class="container">
-        <h1>（ここにユーザー名）</h1>
+        <h1><?php echo $userData['user_name'] ?></h1>
         <div class="row gx-3">
             <div class="col-3 text-center">
-                <img src="./img/user_default.png" class="rounded-circle" width="250" height="250">
+                <img src="<?php
+                            $userpic = glob("./img/userpics/" . $_GET["id"] . "/userpic*");
+                            if ($userpic) {
+                                echo $userpic[0];
+                            } else {
+                                echo "./img/user_default.png";
+                            }
+                            ?>" class="rounded-circle" width="250" height="250">
             </div>
             <div class="col-9">
                 <?php
                 $showIntroduction = true; // 紹介文を表示するかどうかの条件
                 if ($showIntroduction) {
-                    echo "<div>（ここに自己紹介文）</div>";
+                    echo $userData['user_about_me'];
                 } else {
                     // 紹介文を表示しない場合の処理
                 }
@@ -53,4 +60,5 @@ $db = new DBManager;
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="./script/script.js"></script>
 </body>
+
 </html>
