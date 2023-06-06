29ac134386228fa746008800eb0181a8971c050a
Mitsutan <petitcom_hondou@icloud.com>
2023-06-06 12:07:05 +0900

    カード自動生成テスト

---
+++ 04_ソースコード/php/ACGenerator.php
@@ -0,0 +1,46 @@
+<!-- Article Card Generator -->
+<?php
+class ACGenerator
+{
+    public function createCard(int $id, string $title, string $date, string $tags, int $goods)
+    {
+
+        $userpic = glob("./img/userpics/" . $_GET["id"] . "/userpic*");
+        if ($userpic) {
+            $userpic = $userpic[0];
+        } else {
+            $userpic = "./img/user_default.png";
+        }
+
+        echo '
+            <div class="col-6">
+                    <div class="row border-start border-end border-dark border-1 p-2">
+                        <div class="col-7">
+                            <h3>'.$title.'</h3>
+                            <p>'.$date.'</p>
+                            <div>
+                                <div style="display:inline-block;">#ダイエット</div>
+                                <div style="display:inline-block;">#筋トレ</div>
+                                <div style="display:inline-block;">#胸</div>
+                            </div>
+                            <div class="row align-items-end" style="min-height: 10vmax;">
+                                <div class="col-3">
+                                    <img src="' . $userpic . '" class="rounded-circle ratio ratio-1x1">
+                                </div>
+                                <div class="col-9">
+                                    <div>
+                                        <div>ユーザー名</div>
+                                        <div>フォローする</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-5">
+                            <img src="kawaii.jpg" class="w-100 h-100">
+                        </div>
+                    </div>
+                </div>
+            ';
+    }
+}
+?>
--- 04_ソースコード/profile.php
+++ 04_ソースコード/profile.php
@@ -2,7 +2,9 @@
 session_start();
 
 require_once "./php/DBManager.php";
+require_once "./php/ACGenerator.php";
 $db = new DBManager;
+$card = new ACGenerator;
 
 $userData = $db->getUser($_GET["id"]);
 
@@ -28,7 +30,7 @@ $userData = $db->getUser($_GET["id"]);
     <?php require_once "./php/header.php" ?>
     <div class="container">
         <h1><?php echo $userData['user_name'] ?></h1>
-        <div class="row">
+        <div class="row mx-0">
             <div class="col-3 text-center">
                 <div class="rounded-circle ratio ratio-1x1">
                     <img src="<?php
@@ -57,93 +59,58 @@ $userData = $db->getUser($_GET["id"]);
                     <div class="follower-count">フォロワー:</div>
                 </div>
             </div>
+        </div>
 
 
+        <h1 style="margin-top:70px;">投稿記事</h1>
 
-            <h1 style="margin-top:70px;">投稿記事</h1>
-
-            <div class="row gx-5">
-                <div class="col-6">
-                    <div class="row border border-primary">
-                        <div class="col-7">
-                            <h3>記事の見出し</h3>
-                            <p>2023/xx/xx</p>
-                            <div>
-                                <div style="display:inline-block;">#ダイエット</div>
-                                <div style="display:inline-block;">#筋トレ</div>
-                                <div style="display:inline-block;">#胸</div>
-                            </div>
-                            <div class="row align-items-end" style="min-height: 10vmax;">
-                                <div class="col-3">
-                                    <!-- <div class="rounded-circle ratio ratio-1x1 w-25"> -->
-                                    <img src="<?php
-                                                $userpic = glob("./img/userpics/" . $_GET["id"] . "/userpic*");
-                                                if ($userpic) {
-                                                    echo $userpic[0];
-                                                } else {
-                                                    echo "./img/user_default.png";
-                                                }
-                                                ?>" class="rounded-circle ratio ratio-1x1">
-                                    <!-- </div> -->
-                                </div>
-                                <div class="col-9">
-                                    <div>
-                                        <div>ユーザー名</div>
-                                        <div>フォローする</div>
-                                    </div>
-                                </div>
-                            </div>
+        <div class="row g-5 mx-0">
+            <div class="col-6">
+                <div class="row border-start border-end border-dark border-1 p-2">
+                    <div class="col-7">
+                        <h3>記事の見出し</h3>
+                        <p>2023/xx/xx</p>
+                        <div>
+                            <div style="display:inline-block;">#ダイエット</div>
+                            <div style="display:inline-block;">#筋トレ</div>
+                            <div style="display:inline-block;">#胸</div>
                         </div>
-                        <div class="col-5">
-                            <img src="kawaii.jpg" class="w-100 h-100">
+                        <div class="row align-items-end" style="min-height: 10vmax;">
+                            <div class="col-3">
+                                <!-- <div class="rounded-circle ratio ratio-1x1 w-25"> -->
+                                <img src="<?php
+                                            $userpic = glob("./img/userpics/" . $_GET["id"] . "/userpic*");
+                                            if ($userpic) {
+                                                echo $userpic[0];
+                                            } else {
+                                                echo "./img/user_default.png";
+                                            }
+                                            ?>" class="rounded-circle ratio ratio-1x1">
+                                <!-- </div> -->
+                            </div>
+                            <div class="col-9">
+                                <div>
+                                    <div>ユーザー名</div>
+                                    <div>フォローする</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-
-
-
-                <div class="col-6">
-                    <div class="row border border-primary">
-                        <div class="col-7">
-                            <h3>記事の見出し</h3>
-                            <p>2023/xx/xx</p>
-                            <div>
-                                <div style="display:inline-block;">#ダイエット</div>
-                                <div style="display:inline-block;">#筋トレ</div>
-                                <div style="display:inline-block;">#胸</div>
-                            </div>
-                            <div class="row align-items-end" style="min-height: 10vmax;">
-                                <div class="col-3">
-                                    <!-- <div class="rounded-circle ratio ratio-1x1 w-25"> -->
-                                    <img src="<?php
-                                                $userpic = glob("./img/userpics/" . $_GET["id"] . "/userpic*");
-                                                if ($userpic) {
-                                                    echo $userpic[0];
-                                                } else {
-                                                    echo "./img/user_default.png";
-                                                }
-                                                ?>" class="rounded-circle ratio ratio-1x1">
-                                    <!-- </div> -->
-                                </div>
-                                <div class="col-9">
-                                    <div>
-                                        <div>ユーザー名</div>
-                                        <div>フォローする</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-5">
-                            <img src="kawa.jpg" class="w-100 h-100">
-                        </div>
+                    <div class="col-5">
+                        <img src="kawaii.jpg" class="w-100 h-100">
                     </div>
                 </div>
-
-
             </div>
 
+            <?php
+            // foreach ($db->getArticlesByUserId($_GET["id"]) as $key => $value) {
+            //     $card->createCard($value["article_id"], $value["title"], $value["update_datetime"], "", 0);
+            // }
+            ?>
+
         </div>
+
+        <!-- </div> -->
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="./script/script.js"></script>
