d57c3194ae7acec29a83462fedff5f154890d140
Mitsutan <petitcom_hondou@icloud.com>
2023-06-06 09:32:53 +0900

    プロフィール画面
    多分動く

--- 04_ソースコード/profile.php
+++ 04_ソースコード/profile.php
@@ -49,33 +49,33 @@ $userData = $db->getUser($_GET["id"]);
                 } else {
                     // 紹介文を表示しない場合の処理
                 }
-            ?>
-        </div>
-        <div class="col-3" style="text-align:center; padding-left:3%">
-            <div class="follower-box">
-                <div class="follow-count" style="float: left;">フォロー:</div>
-                <div class="follower-count">フォロワー:</div>
+                ?>
             </div>
-        </div>
+            <div class="col-3" style="text-align:center; padding-left:3%">
+                <div class="follower-box">
+                    <div class="follow-count" style="float: left;">フォロー:</div>
+                    <div class="follower-count">フォロワー:</div>
+                </div>
+            </div>
 
 
 
-        <h1 style = "margin-top:70px;">投稿記事</h1>
+            <h1 style="margin-top:70px;">投稿記事</h1>
 
-        <div class="row gx-5">
-            <div class="col-6">
-                <div class="row border border-primary">
-                    <div class="col-7">
-                        <h3>記事の見出し</h3>
-                        <p>2023/xx/xx</p>
-                        <div>
-                            <div style="display:inline-block;">#ダイエット</div>
-                            <div style="display:inline-block;">#筋トレ</div>
-                            <div style="display:inline-block;">#胸</div>
-                        </div>
-                        <div class="row align-items-end" style="min-height: 10vh;">
-                            <div class="col-3">
-                                <!-- <div class="rounded-circle ratio ratio-1x1 w-25"> -->
+            <div class="row gx-5">
+                <div class="col-6">
+                    <div class="row border border-primary">
+                        <div class="col-7">
+                            <h3>記事の見出し</h3>
+                            <p>2023/xx/xx</p>
+                            <div>
+                                <div style="display:inline-block;">#ダイエット</div>
+                                <div style="display:inline-block;">#筋トレ</div>
+                                <div style="display:inline-block;">#胸</div>
+                            </div>
+                            <div class="row align-items-end" style="min-height: 10vmax;">
+                                <div class="col-3">
+                                    <!-- <div class="rounded-circle ratio ratio-1x1 w-25"> -->
                                     <img src="<?php
                                                 $userpic = glob("./img/userpics/" . $_GET["id"] . "/userpic*");
                                                 if ($userpic) {
@@ -84,62 +84,69 @@ $userData = $db->getUser($_GET["id"]);
                                                     echo "./img/user_default.png";
                                                 }
                                                 ?>" class="rounded-circle ratio ratio-1x1">
-                                <!-- </div> -->
-                            </div>
-                            <div class="col-9">
-                                <div>
-                                    <div >ユーザー名</div>
-                                    <div>フォローする</div>
+                                    <!-- </div> -->
+                                </div>
+                                <div class="col-9">
+                                    <div>
+                                        <div>ユーザー名</div>
+                                        <div>フォローする</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        
-                        
-                    </div>
-                    <div class="col-5">
-                        <img src="kawai.jpg" class="w-100 h-100 d-inline-block">
-                        <!-- <p>aaaa</p> -->
+                        <div class="col-5">
+                            <img src="kawaii.jpg" class="w-100 h-100">
+                        </div>
                     </div>
                 </div>
+
+
+
+
+                <div class="col-6">
+                    <div class="row border border-primary">
+                        <div class="col-7">
+                            <h3>記事の見出し</h3>
+                            <p>2023/xx/xx</p>
+                            <div>
+                                <div style="display:inline-block;">#ダイエット</div>
+                                <div style="display:inline-block;">#筋トレ</div>
+                                <div style="display:inline-block;">#胸</div>
+                            </div>
+                            <div class="row align-items-end" style="min-height: 10vmax;">
+                                <div class="col-3">
+                                    <!-- <div class="rounded-circle ratio ratio-1x1 w-25"> -->
+                                    <img src="<?php
+                                                $userpic = glob("./img/userpics/" . $_GET["id"] . "/userpic*");
+                                                if ($userpic) {
+                                                    echo $userpic[0];
+                                                } else {
+                                                    echo "./img/user_default.png";
+                                                }
+                                                ?>" class="rounded-circle ratio ratio-1x1">
+                                    <!-- </div> -->
+                                </div>
+                                <div class="col-9">
+                                    <div>
+                                        <div>ユーザー名</div>
+                                        <div>フォローする</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-5">
+                            <img src="kawa.jpg" class="w-100 h-100">
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
 
-            <div class="col-6">
-                <div class="row border border-primary">
-                    <div class="col-7">
-                        <P>記事の見出し</p>
-                        <p>2023/xx/xx</p>
-                        <div>
-                            <div style="display:inline-block;">#ダイエット</div>
-                            <div style="display:inline-block;">#筋トレ</div>
-                            <div style="display:inline-block;">#胸</div>
-                        </div>
-                        <div class="rounded-circle ratio ratio-1x1 w-25">
-                                <img src="<?php
-                                            $userpic = glob("./img/userpics/" . $_GET["id"] . "/userpic*");
-                                            if ($userpic) {
-                                                echo $userpic[0];
-                                            } else {
-                                                echo "./img/user_default.png";
-                                            }
-                                            ?>" class="rounded-circle ratio ratio-1x1 p-3">
-                        </div>
-                        <div style="padding-left:25%; margin-top:5%;">
-                            <div >ユーザー名</div>
-                            <div>フォローする</div>
-                        </div>
-                    </div>
-                    <div class="col-5">
-                        <img src="kawai.jpg" class="w-100">
-                        <!-- <p>aaaa</p> -->
-                    </div>
-                </div>
-            </div>
         </div>
 
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="./script/script.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="./script/script.js"></script>
 </body>
 
 </html>
