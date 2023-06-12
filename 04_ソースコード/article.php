a12a01b4928b3f522f77b82ab2511884c02b5f36
Mitsutan <petitcom_hondou@icloud.com>
2023-06-12 10:53:25 +0900

    破壊されたコードを極力修正しました
    コンフリクトの修正が正しく行われなかったようで、いくつかのファイルに余計な文字が混入しました。
    極力取り除きましたが、もしかすると手戻りが発生しているかもしれません。

--- 04_ソースコード/article.php
+++ 04_ソースコード/article.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< Updated upstream
 session_start();
 
 require_once "./php/DBManager.php";
@@ -7,9 +6,6 @@ $db = new DBManager();
 
 $articleData = $db->getArticleById($_GET["id"]);
 $userData = $db->getUser($articleData['user_id']);
-=======
-session_start()
->>>>>>> Stashed changes
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -25,18 +21,13 @@ session_start()
 
     <link rel="stylesheet" href="./css/style.css">
 
-<<<<<<< Updated upstream
     <title><?= $articleData['title'] ?></title>
-=======
-    <title>タイトル</title>
->>>>>>> Stashed changes
 </head>
 
 <body>
     <?php require_once "./php/header.php" ?>
 
     <div class="container">
-<<<<<<< Updated upstream
         <h1 class="mb-3"><?= $articleData['title'] ?></h1>
 
         <div class="row">
@@ -47,13 +38,13 @@ session_start()
                     <button type="submit" class="btn btn-warning mb-3 fs-5 px-4">編集</button>
                 </form>
                 <img class="img-fluid img-thumbnail mb-3" src="<?php
-                $topimg = glob("./img/article/" . $articleData['article_id'] . "/topimage*");
-                if ($topimg) {
-                    echo $topimg[0];
-                } else {
-                    echo "./img/article_default.png";
-                }
-                ?>" />
+                                                                $topimg = glob("./img/article/" . $articleData['article_id'] . "/topimage*");
+                                                                if ($topimg) {
+                                                                    echo $topimg[0];
+                                                                } else {
+                                                                    echo "./img/article_default.png";
+                                                                }
+                                                                ?>" />
                 <div class="h3 alert-secondary border border-1 border-dark rounded p-2">
                     <?php
                     // echo "#タグ #を #表示 #します";
@@ -61,7 +52,7 @@ session_start()
                         echo '<a href="./search?type=0&word=' . $value['tag_name'] . '" class="d-inline-block me-1">#' . $value['tag_name'] . '</a>';
                     }
                     ?>
-                    
+
                 </div>
                 <hr aline="center" size="5" class="bg-primary mb-3">
                 <div class="alert-secondary border border-1 border-dark rounded p-2 mb-3">
@@ -75,72 +66,42 @@ session_start()
                 <div class="alert-secondary border border-1 border-dark rounded p-2 mb-3">
                     <h3 class=""> <?php echo "同じ投稿者の次の投稿記事です" ?> </h3>
                 </div>
-=======
-        <h1 class="mb-3">ハッシュタグ一覧</h1>
-
-        <div class="row">
-            <div class="col-8">
-                <a href="./update.php"><button type="submit" class="btn btn-warning mb-3 fs-5">編集</button></a>
-                <img class="img-fluid img-thumbnail mb-3" src="./kawaii.jpg" />
-                <div class="h3 alert-secondary border border-1 border-dark rounded p-2">
-                    <?php echo "#タグ #を #表示 #します" ?>
-                </div>
-                <hr aline="center" size="5" class="bg-primary mb-3">
-                <div class="alert-secondary border border-1 border-dark rounded p-2 mb-2">
-                    <p>
-                        <?php echo "本文を表示----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------" ?>
-                    </p>
-                </div>
->>>>>>> Stashed changes
                 <div class="alert-secondary border border-1 border-dark rounded p-2 mb-2">
                     <h3><?php echo "投稿者プロフィール" ?></h3>
                     <div class="row">
                         <!-- <img src="./kawaii.jpg" class="col-md-5" style="border-radius: 50%; width: 140px; height: 140px;"> -->
                         <div class="col-md-3">
                             <div class="rounded-circle ratio ratio-1x1">
-<<<<<<< Updated upstream
                                 <img src="<?php
-                                $userpic = glob("./img/userpics/" . $userData['user_id'] . "/userpic*");
-                                if ($userpic) {
-                                    echo $userpic[0];
-                                } else {
-                                    echo "./img/user_default.png";
-                                }
-                                ?>" class="rounded-circle ratio ratio-1x1" width="250" height="250">
+                                            $userpic = glob("./img/userpics/" . $userData['user_id'] . "/userpic*");
+                                            if ($userpic) {
+                                                echo $userpic[0];
+                                            } else {
+                                                echo "./img/user_default.png";
+                                            }
+                                            ?>" class="rounded-circle ratio ratio-1x1" width="250" height="250">
                             </div>
                         </div>
                         <div class="col-md-9">
                             <h4><?= $userData['user_name'] ?></h4><br>
                             <p><?= nl2br($userData['user_about_me']) ?></p>
                         </div>
-=======
-                                <img src="kawaii.jpg" class="rounded-circle ratio ratio-1x1" width="250" height="250">
-                            </div>
-                        </div>
-                        <p class="col-md-9"> <?php echo "投稿者名" ?> </p>
->>>>>>> Stashed changes
                     </div>
                 </div>
             </div>
             <div class="col-4">
-<<<<<<< Updated upstream
                 <div class="mb-3">
                     <h2>同じシリーズの記事</h2>
                 </div>
-                <div class="h2 text-center alert-secondary border border-1 border-dark rounded p-5 mb-5" >
-                    <a href="./signup.php" class="mb-2 text-dark" style="font-size: 45px;"> <?php echo "関連記事1日目" ?> </a> 
+                <div class="h2 text-center alert-secondary border border-1 border-dark rounded p-5 mb-5">
+                    <a href="./signup.php" class="mb-2 text-dark" style="font-size: 45px;"> <?php echo "関連記事1日目" ?> </a>
                 </div>
                 <!-- <a href="./signup.php">
                     <div class="h2 text-center alert-secondary border border-1 border-dark rounded p-2 mb-2">
-                        <?php //echo "関連記事１日目" ?>
+                        <?php //echo "関連記事１日目" 
+                        ?>
                     </div>
                 </a> -->
-=======
-                <div class="">
-                    <h2>同じシリーズの記事</h2>
-
-                </div>
->>>>>>> Stashed changes
             </div>
         </div>
     </div>
@@ -149,8 +110,4 @@ session_start()
     <script src="./script/script.js"></script>
 </body>
 
-<<<<<<< Updated upstream
 </html>
-=======
-</html>
->>>>>>> Stashed changes
