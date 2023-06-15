d154195ccbf6e7116b3273ba87d9d82b38e4de3b
Ryuking2525 <r0121moro@gmail.com>
2023-06-15 10:34:13 +0900

    search

---
+++ 04_ソースコード/search.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>筋肉Memorial -検索-</title>
+</head>
+
+<body>
+    <?php require_once "./php/header.php" ?>
+
+    <div class = "from-group">
+        <p class="control-label"><b>対象</b></p>
+        <div class="form-check-inline">
+            <input class="form-check-input" type="radio" name="Radio" id="flexRadioDefault1">
+            <label class="form-check-label" for="Radio1">タグ</label>
+        </div>
+        <div class="form-check-inline">
+            <input class="form-check-input" type="radio" name="Radio" id="flexRadioDefault2" checked>
+            <label class="form-check-label" for="flexRadioDefault2">記事名</label>
+        </div>
+        <div class="form-check-inline">
+            <input class="form-check-input" type="radio" name="Radio" id="flexRadioDefault2" checked>
+            <label class="form-check-label" for="flexRadioDefault2">ユーザー名</label>
+        </div>
+        <div class="form-check-inline">
+            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+            <label class="form-check-label" for="defaultCheck1">フォローのみ</label>
+        </div>
+    </div>
+
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="キーワードを入力">
+        <button class="btn btn-outline-success" type="button" id="button-addon2"><i class="fas fa-search"></i> 検索</button>
+    </div>
+
+    <div class="container">
+        <h1>検索結果</h1>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="./script/script.js"></script>
+</body>
+
+</html>
