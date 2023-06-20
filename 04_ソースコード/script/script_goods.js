fe1838b24dbdc9e4fbd5b3f7c51313673080064b
Mitsutan <petitcom_hondou@icloud.com>
2023-06-20 12:29:51 +0900

    Update script_goods.js
    紛らわしいので消しました

--- 04_ソースコード/script/script_goods.js
+++ 04_ソースコード/script/script_goods.js
@@ -30,26 +30,3 @@ function clickGoods(article) {
     };
     xhr.send("articleNum=" + encodeURIComponent(articleId));
 }
-
-// アンフォローボタンをクリックした時の処理
-function unfollowUser(id) {
-    var unfollowingUserId = id;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "./php/unfollow.php");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            alert(xhr.responseText); // レスポンスの表示（成功メッセージなど）
-            // ボタンの表示を切り替える
-            // document.getElementById("followButtonContainer").innerHTML = '<button onclick="followUser()">フォローする</button>';
-            const fbc = document.getElementsByClassName("followButtonContainer-" + id);
-            for (let i = 0; i < fbc.length; i++) {
-                fbc[i].innerHTML = '<button onclick="followUser(' + id + ')">フォローする</button>';
-            }
-        } else {
-            alert("フォロー解除に失敗しました");
-        }
-    };
-    xhr.send("unfollowingUserId=" + encodeURIComponent(unfollowingUserId));
-}
