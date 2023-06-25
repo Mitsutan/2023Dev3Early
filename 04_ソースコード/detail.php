1305931771aa14f88effc0cb9cd48afa329c48f9
Mitsutan <petitcom_hondou@icloud.com>
2023-06-25 16:04:37 +0900

    Update detail.php
    記事とデザインを統一、他

--- 04_ソースコード/detail.php
+++ 04_ソースコード/detail.php
@@ -6,6 +6,9 @@ $db = new DBManager;
 
 $detailData = $db->getDetailById($_GET['id']);
 $articleData = $db->getArticleById($detailData['article_id']);
+$articleDetails = $db->getDetailsByArticleId($detailData['article_id']);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -30,7 +33,7 @@ $articleData = $db->getArticleById($detailData['article_id']);
     <?php require_once "./php/header.php" ?>
 
     <div class="container">
-        <h1 class="mb-3"><?= $articleData['title'] ?>-1日目-</h1>
+        <h1 class="mb-3"><?= $articleData['title'] ?>-<?= array_search($_GET['id'], array_column($articleDetails, 'detail_id')) + 1 ?>日目-</h1>
 
         <div class="row">
             <div class="col-8">
@@ -93,8 +96,14 @@ $articleData = $db->getArticleById($detailData['article_id']);
                 <div class="mb-3">
                     <h2>同じシリーズの記事</h2>
                 </div>
-                <div class="h2 text-center alert-secondary border border-1 border-dark rounded p-2 mb-2">
-                    <a href="./signup.php" class="mb-2 text-dark"> <?php echo "関連記事1日目" ?> </a>
+                <div class="card">
+                    <ul class="list-group list-group-flush">
+                <?php
+                for ($i=0; $i < count($articleDetails); $i++) { 
+                    echo '<a href="./detail.php?id=' . $articleDetails[$i]['detail_id'] . '"><li class="list-group-item">関連記事' . $i+1 . '日目</li></a>';
+                }
+                ?>
+                    </ul>
                 </div>
             </div>
         </div>
