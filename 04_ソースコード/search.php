d5e05d7deb3ffefbf916973ce86048d05327d5b6
Mitsutan <petitcom_hondou@icloud.com>
2023-07-16 14:19:01 +0900

    検索機能（仮）
    とりあえず検索と、ソートを実装
    「人気順」は単純にいいね総数
    「一週間の人気記事」は定義が分からなかったのでやってない

--- 04_ソースコード/search.php
+++ 04_ソースコード/search.php
@@ -4,8 +4,10 @@ session_start();
 require_once "./php/DBManager.php";
 require_once "./php/search.php";
 require_once "./php/Tbl_mmemorial.php";
-$db = new DBManager;
 $search = new Search;
+require_once "./php/ACGenerator.php";
+$db = new DBManager;
+$card = new ACGenerator;
 
 $userId = $_SESSION['user_id'];
 
@@ -29,108 +31,195 @@ $userId = $_SESSION['user_id'];
 
 <body>
     <?php require_once "./php/header.php" ?>
-    <form action="./search.php" method="post">
     <div class="container">
 
-    <div class = "from-group">
-        <p class="control-label"><b>対象</b></p>
-        <div class="form-check-inline">
-            <input class="form-check-input" type="radio" name="Radio" id="flexRadioDefault1" value="tag">
-            <label class="form-check-label" for="Radio1">タグ</label>
-        </div>
-        <div class="form-check-inline">
-            <input class="form-check-input" type="radio" name="Radio" id="flexRadioDefault2" value="title" checked>
-            <label class="form-check-label" for="flexRadioDefault2">記事名</label>
-        </div>
-        <div class="form-check-inline">
-            <input class="form-check-input" type="radio" name="Radio" id="flexRadioDefault2" value="name" checked>
-            <label class="form-check-label" for="flexRadioDefault2">ユーザー名</label>
-        </div>
-        <div class="form-check-inline">
-            <input class="form-check-input" type="checkbox" value="folow" id="defaultCheck1" name="Check">
-            <label class="form-check-label" for="defaultCheck1">フォローのみ</label>
-        </div>
-    </div>
+        <h1>検索</h1>
+        <form class="mb-2" action="" method="get">
+            <div class="from-group">
+                <p class="control-label fw-bold">対象</p>
+                <div class="form-check-inline">
+                    <input class="form-check-input" type="radio" name="type" value="0" id="flexRadioDefault1" checked>
+                    <label class="form-check-label" for="flexRadioDefault1">タグ</label>
+                </div>
+                <div class="form-check-inline">
+                    <input class="form-check-input" type="radio" name="type" value="1" id="flexRadioDefault2" <?= (isset($_GET['type']) && $_GET['type'] == 1) ? "checked" : "" ?>>
+                    <label class="form-check-label" for="flexRadioDefault2">記事名</label>
+                </div>
+                <div class="form-check-inline">
+                    <input class="form-check-input" type="radio" name="type" value="2" id="flexRadioDefault3" <?= (isset($_GET['type']) && $_GET['type'] == 2) ? "checked" : "" ?>>
+                    <label class="form-check-label" for="flexRadioDefault3">ユーザー名</label>
+                </div>
+                <div class="form-check-inline">
+                    <input class="form-check-input" type="checkbox" name="followOnly" value="y" id="defaultCheck1" <?= isset($_GET['followOnly']) ? "checked" : "" ?>>
+                    <label class="form-check-label" for="defaultCheck1">フォローのみ</label>
+                </div>
 
-    <div class="input-group">
-        <input type="text" class="form-control" placeholder="キーワードを入力" name="keyword">
-        <button class="btn btn-outline-success" id="button-addon2"><i class="fas fa-search"></i> 検索</button>
-    </div>
-    </form>
+                <div class="form-check-inline">
+                    <span>並び替え：</span>
+                    <select name="sort">
+                        <option value="1">新着順</option>
+                        <option value="2" <?= (isset($_GET['sort']) && $_GET['sort'] == 2) ? "selected" : "" ?>>いいね数順</option>
+                        <!-- <option value=”item3”>過去１週間の人気順</option> -->
+                    </select>
+                </div>
+            </div>
+
+            <div class="input-group">
+                <input type="text" class="form-control" name="word" placeholder="キーワードを入力" value="<?= isset($_GET['word']) ? $_GET['word'] : '' ?>">
+                <button class="btn btn-outline-success" type="submit" id="button-addon2"><i class="fas fa-search"></i> 検索</button>
+            </div>
+        </form>
+
 
         <h1>検索結果</h1>
 
-        <div class="row">
-            <div class="col-6">
-                <span>並び替え：</span>
-                <select name=”item”>
-                    <option value=”item1”>人気順</option>
-                    <option value=”item2”>新着順</option>
-                    <option value=”item3”>過去１週間の人気順</option>
-                </select>
+        <div class="<?= (isset($_GET['type'])) ? '' : 'd-none' ?>">
+            <div class="row">
+                <!-- <div class="col-6">
+                    <span>並び替え：</span>
+                    <select name=”item”>
+                        <option value=”item1”>新着順</option>
+                        <option value=”item2”>いいね数順</option>
+                        <option value=”item3”>過去１週間の人気順</option>
+                    </select>
+                </div> -->
+                <div class="col-6">
+                    <p><span id="dataCnt">0</span>件中～<span>0</span>件表示</p>
+                </div>
             </div>
-            <div class="col-6">
-                <p>XXX件中～X件表示</p>
-            </div>
-        </div>
 
-        <div class="row g-5 mx-0">
+            <div class="row g-5 mx-0">
 
-            <div class="col-md-6 col-12">
                 <?php
-                $box = $search -> SearchArticle();
-                $_POST['Keyword'];
-                foreach($box as $id){
-                    echo "ii";  
+                // 検索結果数
+                $dataCnt = 0;
+
+                // 新着順ソートコールバック関数
+                $sort = function ($a, $b) {
+                    return strtotime($b["update_datetime"]) - strtotime($a["update_datetime"]);
+                };
+
+                // いいね数順ソートコールバック関数
+                $sort2 = function ($a, $b) {
+                    return $b["good"] - $a["good"];
+                };
+
+
+                if (isset($_GET['type'])) {
+                    switch ($_GET['type']) {
+                        case '0': // タグ
+                            $restags = $db->getTagsByName($_GET['word']);
+                            $res = [];
+                            foreach ($restags as $key) {
+                                $res = array_merge($res, $db->getArticlesByTagId($key["tag_id"]));
+                            }
+
+                            // 多次元配列の重複を削除
+                            $res = array_map("unserialize", array_unique(array_map("serialize", $res)));
+
+
+                            $sorttype = isset($_GET['sort']) ? $_GET['sort'] : 1;
+                            switch ($sorttype) {
+                                case '1':
+                                    usort($res, $sort);
+                                    break;
+
+                                case '2':
+                                    usort($res, $sort2);
+                                    break;
+
+                                default:
+
+                                    break;
+                            }
+
+                            foreach ($res as $key) {
+                                $tags = $db->getTagsByArticleId($key["article_id"]);
+                                $user = $db->getUser($key["user_id"]);
+                                // $goods = $db->countGoods($key["article_id"]);
+                                $goods = $key["good"];
+                                $isFollowing = (isset($_SESSION['user_id']) ? $db->isFollowingUser($_SESSION['user_id'], $key["user_id"]) : false);
+                                if (isset($_GET['followOnly']) && !$isFollowing) {
+                                    continue;
+                                }
+                                $isGoodsIcon = (isset($_SESSION['user_id']) ? $db->isGoodsIconArticle($_SESSION['user_id'], $key["article_id"]) : false);
+                                $card->createCard($key['article_id'], $key['user_id'], $user['user_name'], $key['title'], $key['update_datetime'], $tags, $goods, $isFollowing, $isGoodsIcon);
+                                $dataCnt++;
+                            }
+                            break;
+
+                        case '1': // 記事名
+                            $res = $db->getArticlesByTitle($_GET['word']);
+                            switch ($_GET['sort']) {
+                                case '1':
+                                    usort($res, $sort);
+                                    break;
+
+                                case '2':
+                                    usort($res, $sort2);
+                                    break;
+
+                                default:
+
+                                    break;
+                            }
+                            foreach ($res as $key) {
+                                $tags = $db->getTagsByArticleId($key["article_id"]);
+                                $user = $db->getUser($key["user_id"]);
+                                $goods = $db->countGoods($key["article_id"]);
+                                $isFollowing = (isset($_SESSION['user_id']) ? $db->isFollowingUser($_SESSION['user_id'], $key["user_id"]) : false);
+                                if (isset($_GET['followOnly']) && !$isFollowing) {
+                                    continue;
+                                }
+                                $isGoodsIcon = (isset($_SESSION['user_id']) ? $db->isGoodsIconArticle($_SESSION['user_id'], $key["article_id"]) : false);
+                                $card->createCard($key['article_id'], $key['user_id'], $user['user_name'], $key['title'], $key['update_datetime'], $tags, $goods, $isFollowing, $isGoodsIcon);
+                                $dataCnt++;
+                            }
+                            break;
+
+                        case '2': // ユーザー名
+                            $resusers = $db->getUserIds($_GET['word']);
+                            $res = [];
+                            foreach ($resusers as $key) {
+                                $res = array_merge($res, $db->getArticlesByUserId($key["user_id"]));
+                            }
+                            switch ($_GET['sort']) {
+                                case '1':
+                                    usort($res, $sort);
+                                    break;
+
+                                case '2':
+                                    usort($res, $sort2);
+                                    break;
+
+                                default:
+
+                                    break;
+                            }
+                            foreach ($res as $key) {
+                                $tags = $db->getTagsByArticleId($key["article_id"]);
+                                $user = $db->getUser($key["user_id"]);
+                                $goods = $db->countGoods($key["article_id"]);
+                                $isFollowing = (isset($_SESSION['user_id']) ? $db->isFollowingUser($_SESSION['user_id'], $key["user_id"]) : false);
+                                if (isset($_GET['followOnly']) && !$isFollowing) {
+                                    continue;
+                                }
+                                $isGoodsIcon = (isset($_SESSION['user_id']) ? $db->isGoodsIconArticle($_SESSION['user_id'], $key["article_id"]) : false);
+                                $card->createCard($key['article_id'], $key['user_id'], $user['user_name'], $key['title'], $key['update_datetime'], $tags, $goods, $isFollowing, $isGoodsIcon);
+                                $dataCnt++;
+                            }
+                            break;
+
+                        default: // 存在しないtype
+                            // exception
+                            break;
+                    }
                 }
-                // $search -> ShowArticle($box);
                 ?>
-                <div class="row border-start border-end border-dark border-1 p-2">
-                    <div class="col-7">
-                        <h3>記事の見出し</h3>
-                        <div class="d-flex justify-content-between">
-                            <p>2023/xx/xx</p>
-                            <p><i class="fa-solid fa-thumbs-up me-1"></i>1234</p>
-                        </div>
-                        <div>
-                            <div style="display:inline-block;">#ダイエット</div>
-                            <div style="display:inline-block;">#筋トレ</div>
-                            <div style="display:inline-block;">#胸</div>
-                        </div>
-                        <div class="row align-items-end" style="min-height: 10vmax;">
-                            <div class="col-4">
-                                <img src="<?php
-                                            // $userpic = glob("./img/userpics/" . $_GET["id"] . "/userpic*");
-                                            // if ($userpic) {  
-                                            //     echo $userpic[0];
-                                            // } else {
-                                                echo "./img/user_default.png";
-                                            // }
-                                            ?>" class="rounded-circle ratio ratio-1x1">
-                            </div>
-                            <div class="col-8">
-                                <div>
-                                    <div>ユーザー名</div>
-                                    <div id="followButtonContainer">
-                                        <?php
-                                        // DBManagerクラスをインスタンス化
-                                        $dbManager = new DBManager();
-                                        $followingUserId = 1;
-                                        // フォロー状態の判定と表示
-                                        $isFollowing = $dbManager->isFollowingUser($userId, $followingUserId);
-                                        if ($isFollowing) {
-                                            echo '<button onclick="unfollowUser()">フォロー解除する</button>';
-                                        } else {
-                                            echo '<button onclick="followUser()">フォローする</button>';
-                                        }
-                                        ?>
-                                    </div>
 
             </div>
-
         </div>
     </div>
-</div>
 
     <nav aria-label="Page navigation example">
         <ul class="pagination justify-content-center">
@@ -149,9 +238,13 @@ $userId = $_SESSION['user_id'];
         </ul>
     </nav>
 
+    <?php require_once "./php/footer.php" ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="./script/script.js"></script>
+    <script>
+        document.getElementById("dataCnt").innerText = <?= $dataCnt ?>;
+    </script>
 
 </body>
 
